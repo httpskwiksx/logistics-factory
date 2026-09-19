a0441f16ef37c2dccf33a76303ca55678fb707db
httpskwikxsi <ulllzzzhaaan@gmail.com>
2026-09-19 18:01:37 +0500

    better for syntax

--- src/logistics/Logistics.java
+++ src/logistics/Logistics.java
@@ -2,9 +2,9 @@ package logistics;
 
 public abstract class Logistics {
     public abstract Transport createTransport();
-public void planDelivery(String cargo, String destination){
+    public void planDelivery(String cargo, String destination){
     Transport transport = createTransport();
     transport.deliver(cargo,destination);
-}
+    }
 }
 
