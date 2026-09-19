900caf7a0710994b0ab5e6c44b39277322f8dbe5
httpskwikxsi <ulllzzzhaaan@gmail.com>
2026-09-19 19:39:25 +0500

    Fix spacing in Ship delivery output

--- src/logistics/Ship.java
+++ src/logistics/Ship.java
@@ -3,7 +3,7 @@ package logistics;
 public class Ship implements Transport {
     @Override
     public void deliver(String cargo, String destination) {
-        System.out.println("Ship delivers" + cargo + " to " + destination);
+        System.out.println("Ship delivers " + cargo + " to " + destination);
     }
 
 }
